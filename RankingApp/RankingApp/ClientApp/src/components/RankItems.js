9be33820c7576296000678f7c6a3f0feafa7350b
Chris Park <chrispark@Chriss-MBP.grandenetworks.net>
2023-09-14 01:01:59 -0500

    resolved RankItems issue with item

--- RankingApp/RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/RankingApp/ClientApp/src/components/RankItems.js
@@ -1,6 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from "./MovieImages.js";
-import RankingGrid from "./RankingGrid";
 
 const RankItems = () => {
 
@@ -17,19 +16,20 @@ const RankItems = () => {
             })
     }, [])
 
+
     return (
         <main>
-            <RankingGrid items={items} imgArr={MovieImageArr} />
-            <div className = "items-not-ranked">
-                (items.length > 0)?items.map((item) =>
-                <div className = "unranked-cell">
-                    <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id === item.imageId)?.image } />
-                </div>
+            <div className="items-not-ranked">
+                {
+                    (items.length > 0) ? items.map((item) =>
+                        <div className="unranked-cell">
+                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image} />
+                        </div>
+                    ) : <div>Loading...</div>
 
-                ):<div>Loading...</div>
+                }
             </div>
         </main>
     )
 }
-
 export default RankItems;
--- RankingApp/RankingApp/Controllers/ItemController.cs
+++ RankingApp/RankingApp/Controllers/ItemController.cs
@@ -20,16 +20,7 @@ namespace RankingApp.Controllers
             new ItemModel{Id =8, Title = "The Shawshank Redemption", ImageId=8, Ranking=0,ItemType=1 },
             new ItemModel{Id =9, Title = "Star Treck IV", ImageId=9, Ranking=0,ItemType=1 },
             new ItemModel{Id =10, Title = "Superman 4", ImageId=10, Ranking=0,ItemType=1 },
-            new ItemModel{Id = 11, Title = "Abbey Road", ImageId=11, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 12, Title = "Adrenalize", ImageId=12, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 13, Title = "Back in Black", ImageId=13, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 14, Title = "Enjoy the Silence", ImageId=14, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 15, Title = "Parachutes", ImageId=15, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 16, Title = "Ride the Lightning", ImageId=16, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 17, Title = "Rock or Bust", ImageId=17, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 18, Title = "Rust in Peace", ImageId=18, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 19, Title = "St. Anger", ImageId=19, Ranking=0,ItemType=2 },
-            new ItemModel{Id = 20, Title = "The Final Countdown", ImageId=20, Ranking=0,ItemType=2 }
+
 
         };
 
