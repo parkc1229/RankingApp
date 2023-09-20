fb2f4094a44a0e9d74c5f47183ea2bad2749baeb
Chris Park <chrispark@Chriss-MBP.grandenetworks.net>
2023-09-20 00:05:23 -0500

    database creation

---
+++ RankingApp/RankingApp/Migrations/20230920044611_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RankingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItemList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ranking = table.Column<int>(type: "INTEGER", nullable: false),
+                    ItemType = table.Column<int>(type: "INTEGER", nullable: false),
+                    RankingsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemList", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemList");
+        }
+    }
+}
--- RankingApp/RankingApp/Models/ItemModel.cs
+++ RankingApp/RankingApp/Models/ItemModel.cs
@@ -1,6 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
 namespace RankingApp.Models
 {
+    public class RankingContext : DbContext
+    {
+        public DbSet<ItemModel> ItemList { get; set; }
+        public DbSet<RankingsModel> RankingsList { get; set; }
+
+        public string DbPath { get; }
+
+        public RankingContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "ranking.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
     public class ItemModel
     {
         public int Id { get; set; }
@@ -9,6 +30,22 @@ namespace RankingApp.Models
         public int Ranking { get; set; }
         public int ItemType { get; set; }
 
+        public int RankedId { get; set; }
+        public RankingsModel RankingsModel { get; set; }
     }
+
+    
+    public class RankingsModel
+    {
+        
+        public int RankingsModelId { get; set; }
+
+        public string Url { get; set; }
+
+        public List<ItemModel> Ranked { get; } = new();
+
+
+    }
+
 }
 
